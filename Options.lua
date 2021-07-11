1b7f15f4061ed13ef44e11128129fdac46a426e6
Mizukichan <mizukichan@arcor.de>
2021-07-11 02:10:16 +0200

    Naming convention

--- Options.lua
+++ Options.lua
@@ -4,7 +4,7 @@
 -- ********************************************************
 --
 -- This addon is written and copyrighted by:
---    * Mîzukichan @ EU-Antonidas (2010-2020)
+--    * Mîzukichan @ EU-Antonidas (2010-2021)
 --
 --
 --    This file is part of Memoria.
